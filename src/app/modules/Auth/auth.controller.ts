f06a1a7eac9458aa5ef84ff2ca41f7e45dfc03eb
DeveloperDolon <dolonr718@gmail.com>
2025-06-03 19:49:33 +0600

    user login controller function added

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,11 +1,18 @@
 import catchAsync from '../../utiils/catchAsync';
+import sendResponse from '../../utiils/sendResponse';
+import { AuthService } from './auth.service';
 
 const login = catchAsync(async (req, res) => {
+  const { username, password } = req.body;
 
-    const { email, password } = req.body;
+  const result = await AuthService.loginUserFromDB({ username, password });
 
-    
-
+  return sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'User logged in successfully',
+    data: result,
+  });
 });
 
 export const AuthController = {
