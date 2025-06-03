a43a2f49f4d7385ed3719309119061707100ae54
DeveloperDolon <dolonr718@gmail.com>
2025-06-03 19:10:39 +0600

    user signup controller added

---
+++ src/app/modules/User/user.controller.ts
@@ -0,0 +1,18 @@
+import catchAsync from '../../utiils/catchAsync';
+import sendResponse from '../../utiils/sendResponse';
+import { UserService } from './user.service';
+
+const signup = catchAsync(async (req, res) => {
+  const result = await UserService.createUserIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
+
+export const UserController = {
+  signup,
+};
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,10 +1,10 @@
 import { TUser } from './user.interface';
 import { User } from './user.model';
 
-const signup = async (payload: TUser) => {
+const createUserIntoDB = async (payload: TUser) => {
   const result = await User.create(payload);
 
   return result;
 };
 
-export const UserService = { signup };
+export const UserService = { createUserIntoDB };
