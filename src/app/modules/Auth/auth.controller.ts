6a8eea4dc2eb002ffa988a1f0e129407a3d2713e
DeveloperDolon <dolonr718@gmail.com>
2025-06-03 20:22:34 +0600

    auth api complete

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -3,9 +3,13 @@ import sendResponse from '../../utiils/sendResponse';
 import { AuthService } from './auth.service';
 
 const login = catchAsync(async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, remember_me } = req.body;
 
-  const result = await AuthService.loginUserFromDB({ username, password });
+  const result = await AuthService.loginUserFromDB({
+    username,
+    password,
+    remember_me,
+  });
 
   return sendResponse(res, {
     statusCode: 200,
