8e8d71a34dec3f002af4e3ab972f3699614823b9
DeveloperDolon <dolonr718@gmail.com>
2025-06-04 07:56:16 +0600

    login method changed

--- src/app/modules/Auth/auth.routes.ts
+++ src/app/modules/Auth/auth.routes.ts
@@ -3,6 +3,6 @@ import { AuthController } from './auth.controller';
 
 const router = express.Router();
 
-router.get('/login', AuthController.login);
+router.post('/login', AuthController.login);
 
 export const AuthRoutes = router;
